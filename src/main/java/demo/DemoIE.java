db9566af8ea89cfd9946c1868f22297315d27a41
baybay24 <mystery_castle1124@hotmail.com>
2019-12-05 22:26:12 +0800

    JDK9_SELENIUM4_TESTNG7

--- src/main/java/demo/DemoIE.java
+++ src/main/java/demo/DemoIE.java
@@ -1,20 +1,37 @@
 package demo;
 
+import browser.InitDriver;
+import elements.ConfigureElement;
+import elements.Element;
+import elements.UserDefineData;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
+
+import java.util.Queue;
 
 public class DemoIE {
 	@Test
 	public static void main() {
 		// TODO Auto-generated method stub
-		String rootPath = new DemoIE().getClass().getResource("").getPath();
-		System.out.print(rootPath);
-		System.setProperty("webdriver.ie.driver", "D:\\software\\driver\\IEDriverServer.exe");
 
-		WebDriver driver = new InternetExplorerDriver();
+
+		WebDriver driver = InitDriver.initBrowser("ie");
 
 		driver.get("http://www.baidu.com");
+
+		Queue<Element> elements = ConfigureElement.getElements(driver);
+
+		UserDefineData.assign(elements);
+
+		driver.findElement(By.className("wd")).sendKeys("limit");
+		driver.findElement(By.id("su")).submit();
+
+
+
+//		driver.close();
 	}
 
 }
